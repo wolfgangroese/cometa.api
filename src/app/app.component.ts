351580cd7ee961d3c45c583676b405cebfb4b330
wolfgangroese <wolfgang.roese@orso.co>
2024-07-08 17:47:58 +0200

    moved menubar in footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from "./shell/header/header.component";
+import {FooterComponent} from "./shell/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [CommonModule, RouterModule, HeaderComponent]
+  imports: [CommonModule, RouterModule, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'cometa';
---
+++ src/app/shell/footer/footer.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterModule, MenubarModule]
+})
+export class FooterComponent implements OnInit {
+  items: any[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      { label: 'Home', icon: 'pi pi-home', routerLink: ['/home'] },
+      { label: 'Todos', icon: 'pi pi-check-circle', routerLink: ['/todos'] },
+      { label: 'Search', icon: 'pi pi-search', routerLink: ['/search'] },
+      { label: 'Nachrichten', icon: 'pi pi-envelope', routerLink: ['/nachrichten'] },
+      { label: 'Account', icon: 'pi pi-user', routerLink: ['/account'] }
+    ];
+  }
+}
