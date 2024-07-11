ff0e7f9c506225b37446ca0de721a2c90cfc669d
wolfgangroese <wolfgang.roese@orso.co>
2024-07-11 06:53:36 +0200

    further small adjustments

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {Todo} from "../../models/todo.model";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  todos: Todo[] = [];
 }
--- src/app/shell/header/header.component.ts
+++ src/app/shell/header/header.component.ts
@@ -1,25 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
-import {MenubarModule} from "primeng/menubar";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   standalone: true,
   styleUrls: ['./header.component.scss'],
-  imports: [CommonModule, RouterModule, MenubarModule]
+  imports: [CommonModule, RouterModule]
 })
-export class HeaderComponent implements OnInit {
-  items: any[] | undefined;
+export class HeaderComponent   {
 
-  ngOnInit() {
-    this.items = [
-      { label: 'Home', icon: 'pi pi-home', routerLink: ['/home'] },
-      { label: 'Todos', icon: 'pi pi-check-circle', routerLink: ['/todos'] },
-      { label: 'Search', icon: 'pi pi-search', routerLink: ['/search'] },
-      { label: 'Nachrichten', icon: 'pi pi-envelope', routerLink: ['/nachrichten'] },
-      { label: 'Account', icon: 'pi pi-user', routerLink: ['/account'] }
-    ];
-  }
 }
