870205c29c5465989253bc722bdcf303356691d3
wolfgangroese <wolfgang.roese@orso.co>
2024-09-01 21:56:35 +0200

    Delete unused backend COMET.API files, update Angular app
    Removed all files related to the unused COMET.API project to clean up the repository. Updated the home component in Angular app and added environment configuration for development mode. This streamlines the project and improves focus on the active codebase.

--- backend/README.md
+++
@@ -1,2 +0,0 @@
-# comet.API
-backend repo for the culture-loving open mechanical turk 1.0
---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,50 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  captcha: {
+    key: '6Lf-AkIdAAAAAEIr-TKvo3KayGf4qTqwmsOuxMLH',
+  },
+  api: {
+    protocol: 'http',
+    baseUrl: 'localhost:5000/api',
+  },
+
+  web: {
+    protocol: 'http',
+    baseUrl: 'localhost:4200',
+  },
+  cometa: {
+    url: 'https://cometa.ws/',
+  },
+  validation: {
+    password: '(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{6,256}',
+    email: '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-z]{2,6}$',
+  },
+  config: {
+    protocol: 'https',
+    baseUrl: 'localhost:5092/swagger/v1/swagger.json',
+  },
+  locale: {
+    default: 'en',
+    locales: {
+      de: 'Deutsch',
+      en: 'English',
+      fr: 'Français',
+      pt: 'Português',
+      ru: 'Русский',
+      ua: 'Українська',
+    },
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
