6f845cc0fbf8bb6f5dfdb34aed26ad796c43b2f7
Wolfgang Roese <mail@wolfgangroese.de>
2025-03-25 19:03:57 +0100

    Enhance account component with user info display and logout functionality

--- src/app/features/account/account.component.ts
+++ src/app/features/account/account.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../models/user.model';
-import {NgIf} from "@angular/common";
-import {RouterLink} from "@angular/router";
+import { NgIf } from "@angular/common";
+import { Router, RouterLink } from "@angular/router";
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-account',
@@ -10,18 +11,27 @@ import {RouterLink} from "@angular/router";
   styleUrls: ['./account.component.scss'],
   imports: [
     NgIf,
-    RouterLink
+    RouterLink,
+    ButtonModule
   ],
   standalone: true
 })
 export class AccountComponent implements OnInit {
   currentUser: User | null = null;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.authService.currentUser$.subscribe((user) => {
       this.currentUser = user;
     });
+  }
+
+ async logout(): Promise<void> {
+    this.authService.logout();
+    await this.router.navigate(['/login']);
   }
 }
